fe52313e43405ecc96ce5bcb58ed48959ca2d328
Rafał Klinowski <klinowskirafal@gmail.com>
2023-03-21 11:25:58 +0100

    Dodano szkielet wzorca API Gateway.

---
+++ PrzetwarzanieObrazow.Web/API/ApiGatewayController.cs
@@ -0,0 +1,37 @@
+﻿namespace PrzetwarzanieObrazow.Web.API;
+
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+public class ApiGatewayController : ControllerBase
+{
+	private readonly HttpClient _httpClient;
+
+	public ApiGatewayController()
+	{
+		_httpClient = new HttpClient();
+	}
+	
+	[HttpGet]
+	[Route("{microserviceName}/{*uri}")]
+	public async Task<HttpResponseMessage> Get(string microserviceName, string uri)
+	{
+		// Forward the HTTP request to the appropriate microservice
+		var request = new HttpRequestMessage(HttpMethod.Get, $"http://{microserviceName}/{uri}");
+		return await _httpClient.SendAsync(request);
+	}
+
+	[HttpPost]
+	[Route("{microserviceName}/{*uri}")]
+	public async Task<HttpResponseMessage> Post(string microserviceName, string uri, [FromBody] object body)
+	{
+		// Forward the HTTP request to the appropriate microservice
+		var request = new HttpRequestMessage(HttpMethod.Post, $"http://{microserviceName}/{uri}")
+		{
+			Content = JsonContent.Create(body, body.GetType())
+		};
+		return await _httpClient.SendAsync(request);
+	}
+}
--- PrzetwarzanieObrazow.Web/API/ImageProcessingController.cs
+++ PrzetwarzanieObrazow.Web/API/ImageProcessingController.cs
@@ -1,27 +1,25 @@
 namespace PrzetwarzanieObrazow.Web.API;
 
+using Code.Algorithms;
+using Code.Builders;
 using Microsoft.AspNetCore.Mvc;
+using Requests;
 
-// https://medium.com/streamwriter/api-gateway-aspnet-core-a46ef259dc54
-// todo: api gateway
-[Route("api/[Controller]")]
-[ApiController]
 public class ImageProcessingController : ControllerBase
 {
-	// [Route("HighPassFilter")]
-	// [HttpPost]
-	// public IActionResult HighPassFilter([FromForm]ImageProcessingRequest request)
-	// {
-	// 	ImageAlgorithm highPassFilter = new HighPassFilter(request.InputImage, request.Width, request.Height);
-	// 	var outputImage = highPassFilter.Process();
-	// 	
-	// 	return Ok(outputImage);
-	// }
-	//
-	// [Route("Test")]
-	// [HttpGet]
-	// public IActionResult Test()
-	// {
-	// 	return Ok("Test");
-	// }
+	[HttpPost(Name = "HighPassFilter")]
+	public IActionResult HighPassFilter([FromForm]ImageProcessingRequest request)
+	{
+		// TODO
+		ImageAlgorithm highPassFilter = new ImageFilterBuilder().BuildHighPassFilterAlgorithm(null);
+		var outputImage = highPassFilter.Process();
+		
+		return Ok(outputImage);
+	}
+	
+	[HttpGet(Name = "Test")]
+	public IActionResult Test()
+	{
+		return Ok("Test");
+	}
 }
